653709b72e5ca4e332a0655f43790828996e0a2f
mshelley <mshelley@deeplearn2.eecs.umich.edu>
2014-12-04 22:57:05 -0500

    added gpu use

--- cnn/cnn_evaluate.m
+++
@@ -1,9 +0,0 @@
-function [rec, prec, ap] = cnn_evaluate(x, y, weights, params)
-
-h = cnn_infer(x, weights, params);
-h2 = cnn_infer2(h, weights, params);
-yhat = cnn_recon(h2, weights, params);
-
-[rec, prec, ap] = compute_ap(yhat(:), y(:));
-
-return;
