ccdccfa3753a67a0200da99c4d0d9a3c6c4fd6fd
kenzshelley <kenzshelley@gmail.com>
2015-01-11 14:19:31 -0800

    removed unnessecary files

--- nate_cnn/run_gradient_then_roll.m
+++
@@ -1,9 +0,0 @@
-function [ cost, grad_vec ] = run_gradient_then_roll( theta, train_data, train_labels, filterSize, numFilters )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-    [cost, grad] = gradient_cnn(theta, train_data, train_labels, filterSize, numFilters, 1);
-    [cost, grad_vec] = roll_params(cost, grad);
-    %numGrad = gradientChecking( @(x) gradient_cnn(x, train_data, train_labels, filterSize, numFilters, 0), theta, grad);
-    
-end
-
--- nate_cnn/validate_cnn.m
+++
@@ -1,9 +0,0 @@
-function [ accuracy ] = validate_cnn( val_data, val_labels, weights, filterSize, numFilters )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    [~, theta] = roll_params(0, weights);
-    [accuracy, ~] = gradient_cnn(theta, val_data, val_labels, filterSize, numFilters, 0);
-    
-
-end
-
